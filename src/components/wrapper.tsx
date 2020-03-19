9f3ef4ea4e11ce435144ba3ef7d24385aa2925dc
doYoungAn <jispoop@gmail.com>
2020-03-19 17:14:21 +0900

    Modal element to use styled-component

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,4 +1,5 @@
 import React, { FC, CSSProperties, useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 export interface IModalWrapperProps {
   cancel: () => void,
@@ -7,46 +8,46 @@ export interface IModalWrapperProps {
 
 const PrefixZIndex: number = 1000;
 
-const ModalBackStyle: CSSProperties = {
-  position: 'fixed',
-  zIndex: PrefixZIndex,
-  top: 0,
-  left: 0,
-  width: '100%',
-  height: '100%',
-  overflow: 'auto',
-  backgroundColor: '#000000',
-  opacity: 0.4
-};
+const ModalBack = styled.div`
+  position: fixed;
+  z-index: ${PrefixZIndex};
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: #000000;
+  opacity: 0.4;
+`;
 
-const ModalContentStyle: CSSProperties = {
-  position: 'fixed',
-  zIndex: PrefixZIndex + 1,
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  background: '#ffffff',
-  padding: '20px',
-};
+const ModalContent = styled.div`
+  position: fixed;
+  z-index: ${PrefixZIndex + 1};
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #ffffff;
+  padding: 20px;
+`;
 
 export const ModalWrapper: FC<IModalWrapperProps> = ({ children, cancel, containerId }): JSX.Element => {
 
-  const [modalBackStyle, setModalBackStyle] = useState<CSSProperties>(ModalBackStyle);
-  const [modalContentStyle, setModalContentStyle] = useState<CSSProperties>(ModalContentStyle);
+  const [modalbackZIndex, setModalbackZIndex] = useState<number>(0);
+  const [modalContentZIndex, setModalContentZIndex] = useState<number>(0);
 
   useEffect(() => {
     const count: number = parseInt(containerId.split('-')[2]);
     const zIndex: number = PrefixZIndex + (count * 2);
-    setModalBackStyle({ ...modalBackStyle, zIndex });
-    setModalContentStyle({ ...modalContentStyle, zIndex: zIndex + 1 });
+    setModalbackZIndex(zIndex);
+    setModalContentZIndex(zIndex + 1);
   }, []);
 
   return (
     <>
-      <div style={modalBackStyle} onClick={() => {cancel()}}></div>
-      <div style={modalContentStyle}>
+      <ModalBack style={{ zIndex: modalbackZIndex }} onClick={() => {cancel()}} />
+      <ModalContent style={{ zIndex: modalContentZIndex }}>
         {children}
-      </div>
+      </ModalContent>
     </>
   )
 }
